5e994dce4e8d3029c9399a2a64d27edee62339b5
kamilgurgener <kamilgurgener@gmail.com>
2018-08-02 16:55:06 -0400

    added health insuranceHealthInsuranceMarketplaceCalculatorPage stateExplanation WebElement

--- src/test/java/org/kff/pages/HealthInsuranceMarketplaceCalculatorPage.java
+++ src/test/java/org/kff/pages/HealthInsuranceMarketplaceCalculatorPage.java
@@ -1,14 +1,21 @@
 package org.kff.pages;
 
 import org.kff.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class HealthInsuranceMarketplaceCalculatorPage {
 	
+	
 			
 	public HealthInsuranceMarketplaceCalculatorPage(){
 		PageFactory.initElements(Driver.getDriver(), this);
 	}	
+		@FindBy(xpath = "//div[@id='state-wrapper']//span[@class='inline-tip']")
+		public WebElement stateExplanation;
+		
+	}	
 
-}
+
