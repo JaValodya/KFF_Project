e017b3ccdae79eca3cf10468b61fa3fed22ea777
fcelikaslan <bujixllc@gmail.com>
2018-08-04 17:45:33 -0400

    new method added

--- src/test/java/org/kff/tests/TestBase.java
+++ src/test/java/org/kff/tests/TestBase.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -79,6 +80,11 @@ public abstract class TestBase {
 			extentLogger.skip("Test Case Skipped is " + result.getName());
 		}
 		Driver.closeDriver();
+	}
+	
+	@Test
+	public void myTest() {
+		driver.get(ConfigurationReader.getProperty("url"));
 	}
 
 	@AfterTest
